7885ace40d946607455a4018494cc8f9ec4f1c41
Семен Киреков <sakireko@mts.ru>
2022-11-12 22:38:54 +0300

    add Product.updateQuantity

--- src/main/java/com/example/demo/domain/Product.java
+++ src/main/java/com/example/demo/domain/Product.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -29,14 +30,19 @@ public class Product {
 
     @NotNull(message = "Product type cannot be null")
     @Enumerated(STRING)
+    @Column(updatable = false)
     private Type type;
 
     @Min(value = 1, message = "Product quantity cannot be less than {value} but provided ${validatedValue}")
     private int quantity;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "fridge_id")
+    @JoinColumn(name = "fridge_id", updatable = false)
     private Fridge fridge;
+
+    public void updateQuantity(int quantity) {
+        setQuantity(quantity);
+    }
 
     public static Product newProduct(Type type, int quantity, Fridge fridge) {
         final var product = new Product();
