93b552d2a04a65cb16ee16a4e331629df741dccf
Семен Киреков <sakireko@mts.ru>
2022-11-12 22:18:03 +0300

    fix DemoApplicationTests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.testutil.IntegrationSuite;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoApplicationTests extends IntegrationSuite {
 
 	@Test
 	void contextLoads() {
