419fa6fffdcbee9113900f65f395d4f9821102ec
Семен Киреков <sakireko@mts.ru>
2022-11-13 13:16:52 +0300

    add FridgeController.createNewFridge

--- src/main/java/com/example/demo/controller/FridgeController.java
+++ src/main/java/com/example/demo/controller/FridgeController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.controller.dto.response.FridgeResponse;
+import com.example.demo.domain.Fridge;
 import com.example.demo.repo.FridgeRepository;
 
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,5 +24,15 @@ public class FridgeController {
         return fridgeRepository.findById(id)
             .map(FridgeResponse::new)
             .orElseThrow();
+    }
+
+    @PostMapping("/api/fridge")
+    @Transactional
+    public FridgeResponse createNewFridge(@RequestParam String name) {
+        return new FridgeResponse(
+            fridgeRepository.save(
+                Fridge.newFridge(name)
+            )
+        );
     }
 }
